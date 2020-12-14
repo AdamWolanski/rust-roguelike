ff759b0507f9910c2fa712b653acfb464c85573f
awolanski <adam.wolanski94@gmail.com>
2020-12-14 23:39:47 +0100

    ✨Feature - basic rltk player movement on map
    + add position, rendering, (temporary)leftmover
    + add basic player movement
    + add basic map
    + modules cleanup
    + create position struct + add use from rltk/specs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,90 @@
-use rltk::{Rltk, GameState};
+mod components;
+mod player;
+mod map;
 
-struct State;
+use rltk::{Rltk, RltkBuilder, GameState};
+use specs::prelude::*;
+
+pub use components::*;
+pub use player::*;
+pub use map::*;
+
+
+pub struct State {
+    ecs : World,
+}
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftMover;
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
+        
         ctx.cls();
-        ctx.print(5, 5, "Hello Rust World");
+        
+        player_input(self, ctx);
+        self.run_systems();
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        map_draw(&map, ctx);
+        
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        
+        for (pos, rend) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, rend.fg, rend.bg, rend.glyph);
+        }
     }
 }
 
+impl<'a> System<'a> for LeftMover {
+    type SystemData = (ReadStorage<'a, LeftMover>,
+                        WriteStorage<'a, Position>);
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 79;
+            }
+        }
+    }
+}
+
+fn components_register(gs : &mut State) {
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
+}
+
 fn main() -> rltk::BError {
-    use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
-    let game_state = State;
+
+    let mut game_state = State {
+        ecs : World::new()
+    };
+
+    components_register(&mut game_state);
+
+    game_state.ecs.insert(new_map_create());
+
+    game_state.ecs
+        .create_entity()
+        .with(Position{
+            x:10, y:10
+        })
+        .with(Renderable{
+            glyph: rltk::to_cp437('@'),
+            fg: rltk::RGB::named(rltk::YELLOW),
+            bg: rltk::RGB::named(rltk::BLACK),
+        })
+        //.with(LeftMover)
+        .with(Player)
+        .build();
+
     rltk::main_loop(context, game_state)
 }
